28a9fb436ec87d93b0e6cbdad090f7ee42ef1210
Patrick Rockenschaub <patrick.rockenschaub.15@ucl.ac.uk>
2020-02-11 15:41:55 +0000

    Add support for random states in RowwiseTransformer

--- extensionarray/reimplement.py
+++ extensionarray/reimplement.py
@@ -5,6 +5,7 @@ from sktime.transformers.segment import IntervalSegmenter, check_is_fitted
 from sktime.transformers.base import BaseTransformer
 from sktime.utils.validation.supervised import validate_X
 
+from sklearn.utils import check_random_state
 
 class RandomIntervalSegmenter(IntervalSegmenter):
     def __init__(self, n_intervals='sqrt', min_length=2, random_state=None):
@@ -44,6 +45,55 @@ class RandomIntervalSegmenter(IntervalSegmenter):
             s.name = f"{colname}_{i[0]}_{i[1]}"
 
         return pd.concat(slices, axis=1)
+
+    @property
+    def random_state(self):
+        return self._random_state
+
+    @random_state.setter
+    def random_state(self, random_state):
+        self._random_state = random_state
+        self._rng = check_random_state(random_state)
+
+    def _rand_intervals_rand_n(self, x):
+        starts = []
+        ends = []
+        m = x.shape[0]  # series length
+        W = self._rng.randint(1, m, size=int(np.sqrt(m)))
+        for w in W:
+            size = m - w + 1
+            start = self._rng.randint(size, size=int(np.sqrt(size)))
+            starts.extend(start)
+            for s in start:
+                end = s + w
+                ends.append(end)
+        return np.column_stack([starts, ends])
+
+    def _rand_intervals_fixed_n(self, x, n_intervals):
+        len_series = len(x)
+        # compute number of random intervals relative to series length (m)
+        # TODO use smarter dispatch at construction to avoid evaluating if-statements here each time function is called
+        if np.issubdtype(type(n_intervals), np.integer) and (n_intervals >= 1):
+            pass
+        elif n_intervals == 'sqrt':
+            n_intervals = int(np.sqrt(len_series))
+        elif n_intervals == 'log':
+            n_intervals = int(np.log(len_series))
+        elif np.issubdtype(type(n_intervals), np.floating) and (n_intervals > 0) and (n_intervals <= 1):
+            n_intervals = int(len_series * n_intervals)
+        else:
+            raise ValueError(f'Number of intervals must be either "random", "sqrt", a positive integer, or a float '
+                             f'value between 0 and 1, but found {n_intervals}.')
+
+        # make sure there is at least one interval
+        n_intervals = np.maximum(1, n_intervals)
+
+        starts = self._rng.randint(len_series - self.min_length + 1, size=n_intervals)
+        if n_intervals == 1:
+            starts = [starts]  # make it an iterable
+
+        ends = [start + self._rng.randint(self.min_length, len_series - start + 1) for start in starts]
+        return np.column_stack([starts, ends])
 
 
 class RowwiseTransformer(BaseTransformer):
