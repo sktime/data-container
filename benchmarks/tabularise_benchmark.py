da60b1acc57cfc1c191ab2b0515e9cd93c71bdc1
Patrick Rockenschaub <patrick.rockenschaub.15@ucl.ac.uk>
2020-02-11 15:44:48 +0000

    Modularise benchmarks

--- benchmarks/tabularise_benchmark.py
+++ benchmarks/tabularise_benchmark.py
@@ -1,24 +1,23 @@
 import timeit
 
-SETUP_CODE = """
-import pandas as pd
+SETUP = """
 import numpy as np
+import pandas as pd
+{packages}
 from sktime.datasets import load_gunpoint
-from sktime.transformers.compose import Tabulariser
 
-# Load the original dataset and create different sizes
-X_small, _ = load_gunpoint(return_X_y=False)
+# Create different sizes of data
+X_base = load_gunpoint(return_X_y=False)
+X_small = {X}
 X_medium = pd.concat([X_small for _ in range(20)])
 X_large = pd.concat([X_small for _ in range(200)])
-"""
 
-SEPARATE_Y = """
+# Choose the size used for this experiment
+X = X_{size}.copy()
+
+# Extract y into a separate vector
 y = X['class_val']
 X.drop('class_val', axis=1, inplace=True)
-"""
-
-CHOOSE_X = """
-X = X_medium.copy()
 """
 
 repeats = 5
@@ -28,9 +27,16 @@ runs = 0
 # **********************************************************************************************************************
 # Time the tabularisation
 
-TEST_CODE = """
+PACKAGES = """
+from sktime.transformers.compose import Tabulariser
+"""
+
+TEST = """
 tab = Tabulariser()
 X_tab = tab.fit_transform(X)
 """
 
-timeit.repeat(setup=SETUP_CODE + CHOOSE_X + SEPARATE_Y, stmt=TEST_CODE, repeat=repeats, number=runs)
+timeit.repeat(setup=SETUP.format(packages=PACKAGES, X="X_base", size="medium"),
+              stmt=TEST,
+              repeat=repeats,
+              number=runs)
