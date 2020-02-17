038d435678c094abee48f0dae702af234efc0fb9
matteogales <matteogales@gmail.com>
2020-02-17 23:35:04 +0000

    Adding code to give initial timings.

--- awkwardarray/benchmarking.py
+++ awkwardarray/benchmarking.py
@@ -1,3 +1,18 @@
+# Import packages and disable certain warnings that impact existing Sktime code
+
+import timeit
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import numpy as np
+
+# Specify how many the provided code should be executed for each timing, and the number of times this process should be repeated
+
+runs = 50
+repeats = 5
+
+# Specify the code that should get run before any timing information is calculated using Awkward Array
+
+AWKWARD_SETUP_CODE = """
 from awkwardarray.transformers import RandomIntervalSegmenter, FeatureUnionTransformer, UniversalFunctionTransformer, GenericFunctionTransformer
 from awkwardarray.utils import awkward_build, awkward_slope_func
 from sklearn.tree import DecisionTreeClassifier
@@ -5,62 +20,237 @@ from sktime.datasets import load_gunpoint
 from sktime.pipeline import Pipeline
 import pandas as pd
 
+X_small = load_gunpoint(return_X_y=False)
+X_medium = pd.concat([X_small for _ in range(20)])
+X_large = pd.concat([X_small for _ in range(200)])
 
-if __name__ == "__main__":
-    # Load the gunpoint dataset and then create different size subsets
+X = X_medium.copy()
+y = X['class_val']
+X.drop('class_val', axis=1, inplace=True)
 
-    X_small = load_gunpoint(return_X_y=False)
-    X_medium = pd.concat([X_small for _ in range(20)])
-    X_large = pd.concat([X_small for _ in range(200)])
+X = awkward_build(X)
+"""
 
-    # Choose a dataset to use for the analysis and separate the class values
+# Apply a random interval segmenter to the dataset using Awkward Array
 
-    X = X_medium.copy()
-    y = X['class_val']
-    X.drop('class_val', axis=1, inplace=True)
+AWKWARD_UP_TO_NOW = AWKWARD_SETUP_CODE
+AWKWARD_TEST_CODE = """
+segmenter = RandomIntervalSegmenter(n_intervals=3)
+X_segmented = segmenter.fit_transform(X)
+"""
+awkward_segment_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_segment_timings = [timing/runs for timing in awkward_segment_timings]
+print(f"\nAwkward Array Segmenter: {awkward_segment_timings}")
 
-    # Convert the nested DataFrame into the JaggedArray to use when testing
+# Calculate the mean for the dataset using Awkward Array
 
-    X = awkward_build(X)
+AWKWARD_UP_TO_NOW += AWKWARD_TEST_CODE
+AWKWARD_TEST_CODE = """
+mean_transformer = UniversalFunctionTransformer("mean")
+X_mean = mean_transformer.fit_transform(X_segmented)
+"""
+awkward_mean_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_mean_timings = [timing/runs for timing in awkward_mean_timings]
+print(f"Awkward Array Mean: {awkward_mean_timings}")
 
-    # Apply a random interval segmenter
+# Calculate the std for the dataset using Awkward Array
 
-    segmenter = RandomIntervalSegmenter("sqrt")
-    X_segmented = segmenter.fit_transform(X)
+AWKWARD_UP_TO_NOW += AWKWARD_TEST_CODE
+AWKWARD_TEST_CODE = """
+std_transformer = UniversalFunctionTransformer("std")
+X_std = std_transformer.fit_transform(X_segmented)
+"""
+awkward_std_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_std_timings = [timing/runs for timing in awkward_std_timings]
+print(f"Awkward Array Std: {awkward_std_timings}")
 
-    # Calculate the mean for the array
+# Calculate the slope for the dataset using Awkward Array
 
-    mean_transformer = UniversalFunctionTransformer("mean")
-    X_mean = mean_transformer.fit_transform(X_segmented)
+AWKWARD_UP_TO_NOW += AWKWARD_TEST_CODE
+AWKWARD_TEST_CODE = """
+slope_transformer = GenericFunctionTransformer(awkward_slope_func, True)
+X_slope = slope_transformer.fit_transform(X_segmented)
+"""
+awkward_slope_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_slope_timings = [timing/runs for timing in awkward_slope_timings]
+print(f"Awkward Array Slope: {awkward_slope_timings}")
 
-    # Calculate the std for the array
+# Combine the above features into a single Numpy array using Awkward Array
 
-    std_transformer = UniversalFunctionTransformer("std")
-    X_std = std_transformer.fit_transform(X_segmented)
+AWKWARD_UP_TO_NOW += AWKWARD_TEST_CODE
+AWKWARD_TEST_CODE = """
+transformers = [('mean', UniversalFunctionTransformer("mean")), ('std', UniversalFunctionTransformer("std")), ('slope', GenericFunctionTransformer(awkward_slope_func, True))]
+feature_transformer = FeatureUnionTransformer(transformers)
+features_combined = feature_transformer.fit_transform(X_segmented)
+"""
+awkward_union_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_union_timings = [timing/runs for timing in awkward_union_timings]
+print(f"Awkward Array Union: {awkward_union_timings}")
 
-    # Calculate the slope for the array
+# Fit a classifier to the combined data to the dataset using Awkward Array
 
-    slope_transformer = GenericFunctionTransformer(awkward_slope_func, True)
-    X_slope = slope_transformer.fit_transform(X_segmented)
+AWKWARD_UP_TO_NOW += AWKWARD_TEST_CODE
+AWKWARD_TEST_CODE = """
+clf = DecisionTreeClassifier()
+clf.fit(features_combined, y)
+"""
+awkward_classifier_timings = timeit.repeat(setup=AWKWARD_UP_TO_NOW, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_classifier_timings = [timing/runs for timing in awkward_classifier_timings]
+print(f"Awkward Array Classifier: {awkward_classifier_timings}")
 
-    # Combine the above features into a single Numpy array
+# Perform all of the above operations into a single pipeline using Awkward Array
 
-    transformers = [('mean', UniversalFunctionTransformer("mean")), ('std', UniversalFunctionTransformer("std")), ('slope', GenericFunctionTransformer(awkward_slope_func, True))]
-    feature_transformer = FeatureUnionTransformer(transformers)
-    features_combined = feature_transformer.fit_transform(X_segmented)
+AWKWARD_TEST_CODE = """
+steps = [
+    ('segment', RandomIntervalSegmenter('sqrt')),
+    ('transform', FeatureUnionTransformer([('mean', UniversalFunctionTransformer("mean")), ('std', UniversalFunctionTransformer("std")), ('slope', GenericFunctionTransformer(awkward_slope_func, True))])),
+    ('clf', DecisionTreeClassifier())
+]
 
-    # Fit a classifier to the combined data
+base_estimator = Pipeline(steps, random_state=1)
+base_estimator.fit(X, y)
+"""
+awkward_pipeline_timings = timeit.repeat(setup=AWKWARD_SETUP_CODE, stmt=AWKWARD_TEST_CODE, repeat=repeats, number=runs)
+awkward_pipeline_timings = [timing/runs for timing in awkward_pipeline_timings]
+print(f"Awkward Array Pipeline: {awkward_pipeline_timings}")
 
-    clf = DecisionTreeClassifier()
-    clf.fit(features_combined, y)
+# Specify the code that should get run before any timing information is calculated using Awkward Array
 
-    # Perform all of the above operations into a single pipeline
+DATAFRAME_SETUP_CODE = """
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import FunctionTransformer
+from sktime.transformers.segment import RandomIntervalSegmenter
+from sktime.transformers.compose import RowwiseTransformer
+from sktime.utils.time_series import time_series_slope
+from sktime.pipeline import Pipeline, FeatureUnion
+from sktime.datasets import load_gunpoint
+import pandas as pd
+import numpy as np
 
-    steps = [
-        ('segment', RandomIntervalSegmenter('sqrt')),
-        ('transform', FeatureUnionTransformer([('mean', UniversalFunctionTransformer("mean")), ('std', UniversalFunctionTransformer("std")), ('slope', GenericFunctionTransformer(awkward_slope_func, True))])),
-        ('clf', DecisionTreeClassifier())
-    ]
+X_small = load_gunpoint(return_X_y=False)
+X_medium = pd.concat([X_small for _ in range(20)])
+X_large = pd.concat([X_small for _ in range(200)])
 
-    base_estimator = Pipeline(steps, random_state=1)
-    base_estimator.fit(X, y)
+X = X_medium.copy()
+y = X['class_val']
+X.drop('class_val', axis=1, inplace=True)
+"""
+
+# Apply a random interval segmenter to the dataset using nested DataFrames
+
+DATAFRAME_UP_TO_NOW = DATAFRAME_SETUP_CODE
+DATAFRAME_TEST_CODE = """
+segmenter = RandomIntervalSegmenter(n_intervals=3)
+X_segmented = segmenter.fit_transform(X)
+"""
+dataframe_segment_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_segment_timings = [timing/runs for timing in dataframe_segment_timings]
+print(f"\nDataFrame Segmenter: {dataframe_segment_timings}")
+
+# Calculate the mean for the dataset using nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+mean_transformer = RowwiseTransformer(FunctionTransformer(func=np.mean, validate=False))
+X_mean = mean_transformer.fit_transform(X_segmented)
+"""
+dataframe_mean_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_mean_timings = [timing/runs for timing in dataframe_mean_timings]
+print(f"DataFrame Mean: {dataframe_mean_timings}")
+
+# Calculate the std for the dataset using nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+std_transformer = RowwiseTransformer(FunctionTransformer(func=np.std, validate=False))
+X_std = std_transformer.fit_transform(X_segmented)
+"""
+dataframe_std_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_std_timings = [timing/runs for timing in dataframe_std_timings]
+print(f"DataFrame Std: {dataframe_std_timings}")
+
+# Calculate the slope for the dataset using nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+slope_transformer = RowwiseTransformer(FunctionTransformer(func=time_series_slope, validate=False))
+X_slope = slope_transformer.fit_transform(X_segmented)
+"""
+dataframe_slope_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_slope_timings = [timing/runs for timing in dataframe_slope_timings]
+print(f"DataFrame Slope: {dataframe_slope_timings}")
+
+# Combine the above features using nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+X_union = pd.concat([X_mean, X_std, X_slope], axis=1)
+"""
+dataframe_union_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_union_timings = [timing/runs for timing in dataframe_union_timings]
+print(f"DataFrame Union: {dataframe_union_timings}")
+
+# Fit a classifier to the combined data to the dataset using nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+clf = DecisionTreeClassifier()
+clf.fit(X_union, y)
+"""
+dataframe_classifier_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_classifier_timings = [timing/runs for timing in dataframe_classifier_timings]
+print(f"DataFrame Classifier: {dataframe_classifier_timings}")
+
+# Perform all of the above operations into a single pipeline nested DataFrames
+
+DATAFRAME_UP_TO_NOW += DATAFRAME_TEST_CODE
+DATAFRAME_TEST_CODE = """
+steps = [
+    ('segment', RandomIntervalSegmenter(n_intervals='sqrt')),
+    ('transform', FeatureUnion([
+        ('mean', RowwiseTransformer(FunctionTransformer(func=np.mean, validate=False))),
+        ('std', RowwiseTransformer(FunctionTransformer(func=np.std, validate=False))),
+        ('slope', RowwiseTransformer(FunctionTransformer(func=time_series_slope, validate=False)))
+    ])),
+    ('clf', DecisionTreeClassifier())
+]
+base_estimator = Pipeline(steps, random_state=1)
+base_estimator.fit(X, y)
+"""
+dataframe_pipeline_timings = timeit.repeat(setup=DATAFRAME_UP_TO_NOW, stmt=DATAFRAME_TEST_CODE, repeat=repeats, number=runs)
+dataframe_pipeline_timings = [timing/runs for timing in dataframe_pipeline_timings]
+print(f"DataFrame Pipeline: {dataframe_pipeline_timings}")
+
+# Obtain the minimum (or typical best case scenario) for each test for each implementation
+
+awkward_segment = np.min(awkward_segment_timings)
+awkward_mean = np.min(awkward_mean_timings)
+awkward_std = np.min(awkward_std_timings)
+awkward_slope = np.min(awkward_slope_timings)
+awkward_union = np.min(awkward_union_timings)
+awkward_classifier = np.min(awkward_classifier_timings)
+awkward_pipeline = np.min(awkward_pipeline_timings)
+
+dataframe_segment = np.min(dataframe_segment_timings)
+dataframe_mean = np.min(dataframe_mean_timings)
+dataframe_std = np.min(dataframe_std_timings)
+dataframe_slope = np.min(dataframe_slope_timings)
+dataframe_union = np.min(dataframe_union_timings)
+dataframe_classifier = np.min(dataframe_classifier_timings)
+dataframe_pipeline = np.min(dataframe_pipeline_timings)
+
+segment_improvement = dataframe_segment / awkward_segment
+mean_improvement = dataframe_mean / awkward_mean
+std_improvement = dataframe_std / awkward_std
+slope_improvement = dataframe_slope / awkward_slope
+union_improvement = dataframe_union / awkward_union
+classifier_improvement = dataframe_classifier / awkward_classifier
+pipeline_improvement = dataframe_pipeline / awkward_pipeline
+
+print(f"\nSegment Multiplier: {segment_improvement}")
+print(f"Mean Multiplier: {mean_improvement}")
+print(f"Std Multiplier: {std_improvement}")
+print(f"Slope Multiplier: {slope_improvement}")
+print(f"Union Multiplier: {union_improvement}")
+print(f"Classifier Multiplier: {classifier_improvement}")
+print(f"Pipeline Multiplier: {pipeline_improvement}")
